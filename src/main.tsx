e0456d28a55a0f331fb00ffb88fdfa82abde5135
Electric Mayonnaise <chilengawarren307@gmail.com>
2026-08-24 19:18:31 +0200

    fix: remove duplicate PWAInstaller to keep single unified InstallBanner

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { registerServiceWorker } from "@/utils/pwa-register";
-import { PWAInstaller } from "@/utils/install-prompt";
 import * as Sentry from "@sentry/react";
 import { initLifecycleTelemetry } from "@/utils/uploadTelemetry";
 
@@ -47,10 +46,6 @@ window.addEventListener("vite:preloadError", (event) => {
 registerServiceWorker({
   onError: (err) => console.warn("[PWA]", err),
 });
-
-new PWAInstaller({
-  onError: (err) => console.warn("[PWAInstaller]", err),
-}).init();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
